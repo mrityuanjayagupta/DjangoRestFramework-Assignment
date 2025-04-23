f6a9f9b2afddf225864d75ecb02cbf35064a8727
mrityuanjayagupta <mrigupta@deloitte.com>
2025-04-24 03:05:11 +0530

    Fixed model to show date_joined in Django Admin

--- task_management_system/core/admin.py
+++ task_management_system/core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register([User])
+
+class UserDjangoAdmin(admin.ModelAdmin):
+    list_display = ("date_joined",)
+    readonly_fields = ("date_joined",)
+
+
+admin.site.register(User, UserDjangoAdmin)
