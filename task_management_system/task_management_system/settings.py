8c898deeae06e955cd8ee4a0e9ff9e6d58d5fed5
mrityuanjayagupta <mrigupta@deloitte.com>
2025-04-24 22:49:56 +0530

    Added JWT Authentication

--- task_management_system/task_management_system/settings.py
+++ task_management_system/task_management_system/settings.py
@@ -36,6 +36,13 @@ REST_FRAMEWORK_ROLES = {
     ],
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    )
+}
+
 AUTH_USER_MODEL = "core.User"
 
 MIDDLEWARE = [
--- task_management_system/task_management_system/urls.py
+++ task_management_system/task_management_system/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
@@ -13,4 +19,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
